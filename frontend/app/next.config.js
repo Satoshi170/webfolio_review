3f3102d49464d0f1dcb1e94540c30af325b65b68
Satoshi170 <sato1704@outlook.jp>
2023-07-10 07:02:15 +0900

    eslint-plugin-jestを追加

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    dirs: ["app", "__tests__"]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
