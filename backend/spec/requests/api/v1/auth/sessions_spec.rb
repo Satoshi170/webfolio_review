a96fedbf7893dc916c1817409059892150256ea7
Satoshi170 <sato1704@outlook.jp>
2023-07-14 04:13:21 +0900

    サインインとサインアウト関連のテストを作成

--- backend/spec/requests/api/v1/auth/sessions_spec.rb
+++ backend/spec/requests/api/v1/auth/sessions_spec.rb
@@ -1,7 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth::Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  describe "GET /auth/sign_in" do
+    context "正しい資格情報を使用した場合" do
+      it "ユーザーがサインインし、認証ヘッダーが返されること" do
+        sign_in(email: user.email, password: user.password)
+        expect(response).to have_http_status(:success)
+        expect(response.headers).to include("access-token", "client", "expiry", "uid")
+        expect(response.body).to include(user.email)
+      end
+    end
+
+    context "間違った資格情報を使用した場合" do
+      it "エラーメッセージが返されること" do
+        invalid_user_params = { email: user.email, password: "incorrect_password" }
+        sign_in(invalid_user_params)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to include("Invalid login credentials. Please try again.")
+      end
+    end
+  end
+
+  describe "DELETE /auth/sign_out" do
+    context "既にログイン状態の場合" do
+      it "ログアウトが成功すること" do
+        sign_in(email: user.email, password: user.password)
+        auth_headers = response.headers.slice("access-token", "client", "expiry", "uid")
+        sign_out(auth_headers)
+        expect(response).to have_http_status(:success)
+        expect(response.body).to include('"success":true')
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "ログアウトが失敗すること" do
+        sign_out
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to include('"success":false')
+      end
+    end
+    # TODO:ログイン状態でないとアクセスできないエンドポイントを実装したときにコメントアウトを外す
+    # context "ログアウトに成功したとき" do
+    #   before do
+    #     sign_in(email: user.email, password: user.password)
+    #     @auth_headers = response.headers.slice("access-token", "client", "expiry", "uid")
+    #     sign_out(@auth_headers)
+    #   end
+
+    #   it "認証トークンが無効になり、アクセスが拒否されること" do
+    #     get "/api/v1/some_endpoint", headers: @auth_headers
+    #     expect(response).to have_http_status(:unauthorized)
+    #   end
+    # end
+  end
+
+  def sign_in(params)
+    post "/api/v1/auth/sign_in", params: params
+  end
+
+  def sign_out(headers = {})
+    delete "/api/v1/auth/sign_out", headers: headers
   end
 end
