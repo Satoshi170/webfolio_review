5350d8a99638de3b9cfac4aba8a97c830dd59817
Satoshi170 <sato1704@outlook.jp>
2023-07-20 06:44:35 +0900

    それぞれ大文字で始まるファイル名に変更

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "./components/footer";
-import Header from "./components/header";
-import Main from "./components/main";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import { Providers } from "./providers";
 
 export const metadata = {
