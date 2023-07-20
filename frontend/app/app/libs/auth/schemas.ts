2df62650058247361f13d81b33c4299a835ddfad
Satoshi170 <sato1704@outlook.jp>
2023-07-21 04:16:33 +0900

    サインアップスキーマに各フィールドのバリデーション制限を追加

---
+++ frontend/app/app/libs/auth/schemas.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+const signUpSchema = z.object({
+  name: z
+    .string()
+    .min(1, "ユーザー名は必須です")
+    .max(25, "ユーザー名は25文字以内である必要があります"),
+  email: z
+    .string()
+    .min(1, "メールアドレスは必須です")
+    .email("無効なメールアドレス形式です"),
+  password: z
+    .string()
+    .min(1, "パスワードは必須です")
+    .min(6, "パスワードは6文字以上である必要があります"),
+  passwordConfirmation: z.string()
+});
+
+export const refinedSignUpSchema = signUpSchema.refine(
+  (data) => data.password === data.passwordConfirmation,
+  {
+    message: "パスワードと確認用パスワードが一致しません",
+    path: ["passwordConfirmation"]
+  }
+);
