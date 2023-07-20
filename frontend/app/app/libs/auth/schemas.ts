b3eda86d02bca893573c3ff6f11a21037c0ae3f6
Satoshi170 <sato1704@outlook.jp>
2023-07-21 05:33:34 +0900

    サインインスキーマに各フィールドのバリデーション制限を追加

--- frontend/app/app/libs/auth/schemas.ts
+++ frontend/app/app/libs/auth/schemas.ts
@@ -23,3 +23,11 @@ export const refinedSignUpSchema = signUpSchema.refine(
     path: ["passwordConfirmation"]
   }
 );
+
+export const signInSchema = z.object({
+  email: z
+    .string()
+    .min(1, "メールアドレスを入力してください")
+    .email("無効なメールアドレス形式です"),
+  password: z.string().min(1, "パスワードを入力してください")
+});
