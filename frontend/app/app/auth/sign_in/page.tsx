e8220af477b2240df9cf531d7e3cac9139a007be
Satoshi170 <sato1704@outlook.jp>
2023-07-21 17:04:01 +0900

    サインイン、サインアップページを作成

---
+++ frontend/app/app/auth/sign_in/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Box, Divider, Heading, Link, Text } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import NextLink from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import InputField from "@/app/components/auth/InputField";
+import SubmitButton from "@/app/components/auth/SubmitButton";
+import { signInSchema } from "@/app/libs/auth/schemas";
+import { signIn } from "@/app/libs/auth/signIn";
+import { SignInCredentials } from "@/app/types/auth";
+
+const SignIn: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignInCredentials>({
+    resolver: zodResolver(signInSchema),
+    mode: "onChange"
+  });
+
+  const onSubmit = (data: SignInCredentials) => {
+    setLoading(true);
+    signIn(data)
+      .then(() => {
+        router.push("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <Box flex="1" m="auto" maxW="md" boxShadow="md" p="12" rounded="md">
+      <Heading as="h2" textAlign="center" mb="4">
+        ログイン
+      </Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputField
+          name="email"
+          label="メールアドレス"
+          register={register}
+          error={errors.email}
+        />
+        <InputField
+          name="password"
+          label="パスワード"
+          type="password"
+          register={register}
+          error={errors.password}
+        />
+        <SubmitButton text="ログイン" isLoading={loading} />
+        <Divider my="4" />
+        <Text>
+          初めてのご利用ですか？新規登録は
+          <Link as={NextLink} href="/auth/sign_up" color="blue">
+            こちら
+          </Link>
+        </Text>
+      </form>
+    </Box>
+  );
+};
+
+export default SignIn;
---
+++ frontend/app/app/auth/sign_up/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Box, Divider, Heading, Text, Link } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import NextLink from "next/link";
+import { useForm } from "react-hook-form";
+
+import InputField from "@/app/components/auth/InputField";
+import SubmitButton from "@/app/components/auth/SubmitButton";
+import { useAuthForm } from "@/app/hooks/auth/useAuthForm";
+import { refinedSignUpSchema } from "@/app/libs/auth/schemas";
+import { signUp } from "@/app/libs/auth/signUp";
+import { SignUpCredentials } from "@/app/types/auth";
+
+const SignUp: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignUpCredentials>({
+    resolver: zodResolver(refinedSignUpSchema),
+    mode: "onChange"
+  });
+
+  const { onSubmit, isLoading } = useAuthForm(signUp, "/user/account");
+
+  return (
+    <Box flex="1" m="auto" maxW="md" boxShadow="md" p="12" rounded="md">
+      <Heading as="h2" textAlign="center" mb="4">
+        アカウント作成
+      </Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputField
+          name="name"
+          label="ユーザー名"
+          register={register}
+          error={errors.name}
+        />
+        <InputField
+          name="email"
+          label="メールアドレス"
+          register={register}
+          error={errors.email}
+        />
+        <InputField
+          name="password"
+          label="パスワード"
+          type="password"
+          register={register}
+          error={errors.password}
+        />
+        <InputField
+          name="passwordConfirmation"
+          label="パスワード(再確認)"
+          type="password"
+          register={register}
+          error={errors.passwordConfirmation}
+        />
+        <SubmitButton text="登録する" isLoading={isLoading} />
+        <Divider my="4" />
+        <Text>
+          すでにアカウントをお持ちですか？
+          <Link as={NextLink} href="/auth/sign_in" color="blue">
+            サインイン
+          </Link>
+        </Text>
+      </form>
+    </Box>
+  );
+};
+
+export default SignUp;
