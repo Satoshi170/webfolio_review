7e25c2df73cae7aa3866d694e450a830fc7e795d
Satoshi170 <sato1704@outlook.jp>
2023-07-21 16:54:05 +0900

    recoilの初期設定

--- frontend/app/app/providers.tsx
+++ frontend/app/app/providers.tsx
@@ -2,13 +2,16 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
 
 import theme from "./theme/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </CacheProvider>
+    <RecoilRoot>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </CacheProvider>
+    </RecoilRoot>
   );
 }
