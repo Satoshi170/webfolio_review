2cbc6259cdddd0fb7d7bfef63abb7edd5752cba7
Satoshi170 <sato1704@outlook.jp>
2023-07-23 16:25:20 +0900

    レスポンスから得られた認証情報をCookieに保存するヘルパー関数を作成

---
+++ frontend/app/app/libs/cookie/saveAuthInfo.ts
@@ -0,0 +1,13 @@
+import Cookies from "js-cookie";
+
+import { CustomAxiosResponse } from "@/app/types/axios/customAxiosResponse";
+
+export const saveAuthInfoFromHeader = (response: CustomAxiosResponse) => {
+  const token = response.headers["access-token"];
+  const client = response.headers["client"];
+  const uid = response.headers["uid"];
+
+  Cookies.set("access-token", token, { secure: true, sameSite: "lax" });
+  Cookies.set("client", client, { secure: true, sameSite: "lax" });
+  Cookies.set("uid", uid, { secure: true, sameSite: "lax" });
+};
