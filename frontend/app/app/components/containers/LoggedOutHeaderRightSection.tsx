4a2082c37163b3926eca430f4c2cf2f1ba5d961e
Satoshi170 <sato1704@outlook.jp>
2023-07-23 19:21:22 +0900

    ログアウト時はボタンからサインインページ,新規登録ページに行けるようにした

--- frontend/app/app/components/containers/LoggedOutHeaderRightSection.tsx
+++ frontend/app/app/components/containers/LoggedOutHeaderRightSection.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Spacer } from "@chakra-ui/react";
 
 const LoggedOutHeaderRightSection: React.FC = () => {
   return (
-    <Flex>
-      <Button>ログイン</Button>
-      <Button>サインイン</Button>
+    <Flex align="center">
+      <Button as="a" href="/auth/sign_up" colorScheme="messenger">
+        新規登録
+      </Button>
+      <Spacer mx="1" />
+      <Button as="a" href="/auth/sign_in" colorScheme="messenger">
+        サインイン
+      </Button>
     </Flex>
   );
 };
