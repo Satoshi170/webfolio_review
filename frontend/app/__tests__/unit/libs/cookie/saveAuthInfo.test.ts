dc1d0b058d003c8b34a9ebf614887d4814db8375
Satoshi170 <sato1704@outlook.jp>
2023-07-27 22:08:10 +0900

    saveAuthInfoFromHeaderの単体テストを作成

---
+++ frontend/app/__tests__/unit/libs/cookie/saveAuthInfo.test.ts
@@ -0,0 +1,40 @@
+import Cookies from "js-cookie";
+
+import { saveAuthInfoFromHeader } from "@/app/libs/cookie/saveAuthInfo";
+import { CustomAxiosResponse } from "@/app/types/axios/customAxiosResponse";
+
+jest.mock("js-cookie");
+
+describe("saveAuthInfoFromHeader", () => {
+  it("ヘッダーから認証情報を取得し、クッキーに保存する", () => {
+    const mockResponse: CustomAxiosResponse = {
+      headers: {
+        "access-token": "mockToken",
+        client: "mockClient",
+        uid: "mockUid"
+      },
+      data: {},
+      status: 0,
+      statusText: "OK",
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      config: {} as any
+    };
+
+    const setSpy = jest.spyOn(Cookies, "set");
+
+    saveAuthInfoFromHeader(mockResponse);
+
+    expect(setSpy).toHaveBeenCalledWith("access-token", "mockToken", {
+      secure: true,
+      sameSite: "lax"
+    });
+    expect(setSpy).toHaveBeenCalledWith("client", "mockClient", {
+      secure: true,
+      sameSite: "lax"
+    });
+    expect(setSpy).toHaveBeenCalledWith("uid", "mockUid", {
+      secure: true,
+      sameSite: "lax"
+    });
+  });
+});
