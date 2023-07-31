ebb1fb12fb9cf90f377bfd0aee38eca2ed29dede
Satoshi170 <sato1704@outlook.jp>
2023-07-31 17:53:06 +0900

    ボタン押下時に再レンダリングされないようにした

--- frontend/app/app/components/containers/LoggedOutHeaderRightSection.tsx
+++ frontend/app/app/components/containers/LoggedOutHeaderRightSection.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import { Button, Flex, Spacer } from "@chakra-ui/react";
+import { Button, Flex, Spacer, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const LoggedOutHeaderRightSection: React.FC = () => {
   return (
     <Flex align="center" data-testid="logged-out-header">
-      <Button as="a" href="/auth/sign_up" colorScheme="messenger">
-        新規登録
-      </Button>
+      <Link as={NextLink} href="/auth/sign_up">
+        <Button
+          as="a"
+          colorScheme="messenger"
+          sx={{ "&:hover": { textDecoration: "none" } }}
+        >
+          新規登録
+        </Button>
+      </Link>
       <Spacer mx="1" />
-      <Button as="a" href="/auth/sign_in" colorScheme="messenger">
-        サインイン
-      </Button>
+      <Link as={NextLink} href="/auth/sign_in">
+        <Button
+          as="a"
+          colorScheme="messenger"
+          sx={{ "&:hover": { textDecoration: "none" } }}
+        >
+          サインイン
+        </Button>
+      </Link>
     </Flex>
   );
 };
