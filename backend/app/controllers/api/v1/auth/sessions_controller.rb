dfb215abece27e016ceca2926f1b8f065fd1106f
Satoshi170 <sato1704@outlook.jp>
2023-08-02 14:06:10 +0900

    Userモデルに画像添付機能と画像URL生成メソッドを追加

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
     if current_api_v1_user
-      render json: { is_login: true, data: current_api_v1_user.as_json(only: [:name, :image]) }
+      user_data = current_api_v1_user.as_json(only: [:name]).merge(image: current_api_v1_user.image_url)
+      render json: { is_login: true, data: user_data }
     else
       render json: { is_login: false }
     end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+	has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  def image_url
+    if image.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(image, only_path: false)
+    else
+      "#{Rails.application.routes.default_url_options[:protocol]}://#{Rails.application.routes.default_url_options[:host]}:#{Rails.application.routes.default_url_options[:port]}/images/default_user_image.png"
+    end
+  end
 
   validates :name,
             presence: { message: 'Name is required.' },
