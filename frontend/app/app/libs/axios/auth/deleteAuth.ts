6f8f05f38894f985a5e51f77155b06ddcdef2311
Satoshi170 <sato1704@outlook.jp>
2023-08-13 00:56:24 +0900

    deleteAuthの単体テストを作成

--- frontend/app/app/libs/axios/auth/deleteAuth.ts
+++ frontend/app/app/libs/axios/auth/deleteAuth.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 
 import api from "@/app/libs/axios/api";
 import { addAuthInfoToRequest } from "@/app/libs/cookie/loadAuthInfo";
-import { DeleteAuthSuccessData } from "@/app/types/axios/auth/deleteAuth";
+import {
+  DeleteAuthErrorData,
+  DeleteAuthSuccessData
+} from "@/app/types/axios/auth/deleteAuth";
 
 import { removeAuthInfo } from "../../cookie/removeAuthInfo";
 
@@ -11,8 +14,10 @@ export const deleteAuth = async (): Promise<void> => {
     await api.delete<DeleteAuthSuccessData>("/auth", addAuthInfoToRequest({}));
     removeAuthInfo();
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      throw new Error(error.message);
+    if (axios.isAxiosError(error) && error.response) {
+      const errorResponseData = error.response.data as DeleteAuthErrorData;
+      const errorMessage = errorResponseData.errors.join(", ");
+      throw new Error(errorMessage);
     } else {
       throw error;
     }
