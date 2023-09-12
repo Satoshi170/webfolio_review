62d44ecafa3339fb07d353bc50e8e6385563c8df
Satoshi170 <sato1704@outlook.jp>
2023-09-12 12:53:19 +0900

    undefinedの時にエラーが発生しないように修正

--- frontend/app/app/libs/axios/api.ts
+++ frontend/app/app/libs/axios/api.ts
@@ -5,12 +5,8 @@ const options = {
   ignoreHeaders: true
 };
 
-const baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT;
-if (!baseURL) {
-  throw new Error("API endpoint is not defined");
-}
 const api = axios.create({
-  baseURL: `${baseURL}/api/v1`
+  baseURL: `${process.env.NEXT_PUBLIC_API_ENDPOINT || "default_endpoint"}/api/v1`
 });
 
 applyCaseMiddleware(api, options);
