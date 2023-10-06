e59d8b84a735e680335208ab8d16e8018168f285
Satoshi170 <sato1704@outlook.jp>
2023-10-06 17:42:50 +0900

    Linkifyを導入しリンク要素の生成方法をオーバーライドできるようにした

---
+++ frontend/app/app/components/atoms/CustomLink.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Link, LinkProps } from "@chakra-ui/react";
+
+const CustomLink = (props: LinkProps): JSX.Element => {
+  const { href, children, ...other } = props;
+
+  return (
+    <Link {...other} href={href} target="_blank" rel="noopener noreferrer" color="blue">
+      {children}
+    </Link>
+  );
+};
+
+export default CustomLink;
