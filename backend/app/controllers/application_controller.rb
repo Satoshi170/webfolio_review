79af2094a5cab7b745bff9631426f51977847aa0
Satoshi170 <sato1704@outlook.jp>
2023-10-15 06:31:12 +0900

    ApplicationControllerにActiveRecord::RecordNotFoundのグローバルハンドリングを追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render json: { status: "error", message: "Record not found" }, status: :not_found
+  end
 end
