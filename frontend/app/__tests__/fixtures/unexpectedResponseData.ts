349ee3ccea7effb976d4673b6712aeee5b701fad
Satoshi170 <sato1704@outlook.jp>
2023-10-16 00:56:37 +0900

    テスト用のresponseDataを作成

---
+++ frontend/app/__tests__/fixtures/unexpectedResponseData.ts
@@ -0,0 +1,7 @@
+export const mockUnexpectedResponseData = {
+  unexpected: ["unexpectedResponse"]
+};
+
+export const mockUnexpectedResponse = {
+  response: { data: mockUnexpectedResponseData }
+};
