81d43082f639e0c0f19a9059a88a65d8861d3836
Satoshi170 <sato1704@outlook.jp>
2023-10-15 23:05:15 +0900

    エラーメッセージに関する定数を配置するerrorsフォルダを作成しそこに移動

--- frontend/app/app/constants/portfolio/postPortfolio/Messages.ts
+++
@@ -1,6 +0,0 @@
-export const postPortfolioValidationErrorMessages = {
-  titleRequired: "タイトルを入力してください",
-  titleTooLong: "タイトルは25文字以内である必要があります",
-  contentRequired: "投稿内容を入力してください",
-  contentTooLong: "投稿内容は255文字以内である必要があります"
-};
