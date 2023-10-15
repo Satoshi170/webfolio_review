f0ab00ea6c9383633549ced6c88636092ceff32a
Satoshi170 <sato1704@outlook.jp>
2023-10-16 01:03:28 +0900

    API通信のエラーレスポンスをスキーマで定義するフォルダを作成しスキーマより型を生成

--- frontend/app/app/types/auth.ts
+++ frontend/app/app/types/auth.ts
@@ -1,5 +1,11 @@
+import { z } from "zod";
+
+import { UnauthorizedResponseDataSchema } from "../libs/zod/apiErrorResponses/auth/responseDataSchema";
+
 export interface UserData {
   id: number;
   name: string;
   image: string;
 }
+
+export type UnauthorizedResponseData = z.infer<typeof UnauthorizedResponseDataSchema>;
