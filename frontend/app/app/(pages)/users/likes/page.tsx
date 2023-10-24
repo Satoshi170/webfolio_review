cc6dfff017f97835e206025853816ff39aaa9d1b
Satoshi170 <sato1704@outlook.jp>
2023-10-25 04:20:18 +0900

    /users/likesに表示されるページコンポーネントを作成

---
+++ frontend/app/app/(pages)/users/likes/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Suspense } from "react";
+import { useRecoilValue } from "recoil";
+
+import WithRedirectIfLoggedOut from "@/app/components/HOCs/WithRedirectIfLoggedOut";
+import LoadingSpinner from "@/app/components/atoms/LoadingSpinner";
+import PostCard from "@/app/components/organisms/posts/PostCard";
+import CenteredBox from "@/app/components/styledWrappers/CenteredBox";
+import { useGetPortfoliosOperation } from "@/app/hooks/operations/portfolio/useGetPortfoliosOperation";
+import { LoggedInState, loginState } from "@/app/stores/atoms/loginState";
+
+const UserLikesPage: React.FC = () => {
+  const { userData } = useRecoilValue(loginState) as LoggedInState;
+  const ids = userData.goods.map((obj) => obj.portfolioId);
+  const portfoliosData = useGetPortfoliosOperation(ids);
+
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <CenteredBox>
+        {portfoliosData.map((portfolioData, i) => (
+          <PostCard
+            portfolioData={portfolioData}
+            linkOptions={{ header: true, body: true }}
+            key={i}
+          />
+        ))}
+      </CenteredBox>
+    </Suspense>
+  );
+};
+
+export default WithRedirectIfLoggedOut(UserLikesPage);
