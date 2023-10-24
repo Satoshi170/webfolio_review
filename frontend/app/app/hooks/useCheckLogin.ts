f3dbecaa6b960e92b9080a7a5b03ded6ad35bbbd
Satoshi170 <sato1704@outlook.jp>
2023-10-25 02:19:48 +0900

    APIレスポンス内容の変更に伴い修正

--- frontend/app/app/hooks/useCheckLogin.ts
+++ frontend/app/app/hooks/useCheckLogin.ts
@@ -13,10 +13,10 @@ export const useCheckLogin = () => {
     try {
       const responseData = await getAuthSessions();
       if (responseData.isLogin) {
-        const { id, name, image } = responseData.data;
+        const { id, name, image, goods } = responseData.data;
         setLogin({
           isLogin: true,
-          userData: { id, name, image: image || defaultUserImage }
+          userData: { id, name, goods, image: image || defaultUserImage }
         });
       } else {
         setLogin({ isLogin: false, userData: null });
