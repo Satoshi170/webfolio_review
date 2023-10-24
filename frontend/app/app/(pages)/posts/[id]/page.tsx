9629371062245286f1cfc1e3fecd15aaa09bf235
Satoshi170 <sato1704@outlook.jp>
2023-10-25 00:57:07 +0900

    カスタムフック側の引数をidにするように修正

--- frontend/app/app/(pages)/posts/[id]/page.tsx
+++ frontend/app/app/(pages)/posts/[id]/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
 import Error from "next/error";
 import { usePathname } from "next/navigation";
 
 import GoBackLink from "@/app/components/atoms/GoBackLink";
 import LoadingSpinner from "@/app/components/atoms/LoadingSpinner";
 import PostCard from "@/app/components/organisms/posts/PostCard";
+import CenteredBox from "@/app/components/styledWrappers/CenteredBox";
 import { useGetPortfoliosByIdOperation } from "@/app/hooks/operations/portfolio/useGetPortfoliosByIdOperation";
+import { getIdOrTriggerNotFound } from "@/app/utils/getIdOrTriggerNotFound";
 
 const PostPageById: React.FC = () => {
   const pathname = usePathname();
-  const { status, portfolioData } = useGetPortfoliosByIdOperation(pathname);
+  const id = getIdOrTriggerNotFound({ pathname, routeKey: "posts" });
+  const { status, portfolioData } = useGetPortfoliosByIdOperation(id);
 
   if (!status) {
     return <LoadingSpinner />;
@@ -19,10 +21,10 @@ const PostPageById: React.FC = () => {
 
   if (status === 200 && portfolioData) {
     return (
-      <Box h="auto" w="auto" maxW="lg" alignItems="center" mx="auto">
+      <CenteredBox>
         <GoBackLink />
         <PostCard portfolioData={portfolioData} linkOptions={{ header: true }} />
-      </Box>
+      </CenteredBox>
     );
   } else {
     return <Error statusCode={status} />;
