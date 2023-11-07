da6dfe72bdca7eaf98b758dffe513802cd07d4ed
Satoshi170 <sato1704@outlook.jp>
2023-11-07 14:34:14 +0900

    Commentモデルを作成

--- backend/app/models/portfolio.rb
+++ backend/app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :goods, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
 
   validates :title,
             presence: true,
