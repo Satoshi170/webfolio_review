b15d59ba11262f7a386468c76595595596145d5f
Satoshi170 <sato1704@outlook.jp>
2023-11-08 01:02:36 +0900

    Commentモデルのバリデーションを設定

--- backend/spec/models/comment_spec.rb
+++ backend/spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validation" do
+    describe "content" do
+      it "contentが存在する必要がある" do
+        comment = Comment.new(content: nil)
+        expect(comment).not_to be_valid
+        expect(comment.errors[:content]).to include("can't be blank")
+      end
+
+      it "contentの長さは255文字以内である必要がある" do
+        comment = Comment.new(content: 'a' * 256)
+        expect(comment).not_to be_valid
+        expect(comment.errors[:content]).to include("is too long (maximum is 255 characters)")
+      end
+    end
+  end
 end
