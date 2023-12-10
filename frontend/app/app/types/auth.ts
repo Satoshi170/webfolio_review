d53090058af4e00960a1a41921fb31996bd198a3
Satoshi170 <sato1704@outlook.jp>
2023-12-11 05:40:10 +0900

    /auth/sessionsの仕様変更に伴いフロンド側も修正

--- frontend/app/app/types/auth.ts
+++ frontend/app/app/types/auth.ts
@@ -5,12 +5,19 @@ interface Good {
   portfolioId: number;
 }
 
+interface Comment {
+  portfolioId: number;
+  content: string;
+  updatedAt: Date;
+}
+
 export interface UserData {
   id: number;
   name: string;
   role: "user" | "guest";
   image: string;
   goods: Good[];
+  comments: Comment[];
 }
 
 export type UnauthorizedResponseData = z.infer<typeof UnauthorizedResponseDataSchema>;
