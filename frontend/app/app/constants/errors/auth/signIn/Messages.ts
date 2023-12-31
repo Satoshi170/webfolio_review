6be97e59112cf3d37519f385680f6ceffde12d82
Satoshi170 <sato1704@outlook.jp>
2023-12-31 12:46:58 +0900

    signInValidationMessageに新しいプロパティを作成しそれを使用するように修正

--- frontend/app/app/constants/errors/auth/signIn/Messages.ts
+++ frontend/app/app/constants/errors/auth/signIn/Messages.ts
@@ -1,5 +1,6 @@
 export const signInValidationErrorMessages = {
   emailRequired: "メールアドレスを入力してください",
   invalidEmail: "無効なメールアドレス形式です",
-  passwordRequired: "パスワードを入力してください"
+  passwordRequired: "パスワードを入力してください",
+  invalidParams: "メールアドレスまたはパスワードが違います"
 };
