007c2774e89276b97b6251d0704ef483974cd48f
Satoshi170 <sato1704@outlook.jp>
2024-01-04 02:48:36 +0900

    validUserDataを更新

--- frontend/app/__tests__/fixtures/auth/validUserData.ts
+++ frontend/app/__tests__/fixtures/auth/validUserData.ts
@@ -3,5 +3,8 @@ import { UserData } from "@/app/types/auth";
 export const validUserData: UserData = {
   id: 1,
   name: "testuser",
-  image: "/defaultUserImage.png"
+  role: "user",
+  image: "/defaultUserImage.png",
+  goods: [],
+  comments: []
 };
