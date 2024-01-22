e8beb2208ab63112c0da0b1531f373bcd306901b
Satoshi170 <sato1704@outlook.jp>
2024-01-22 14:48:24 +0900

    useGetMetaDataをuseSWRWithAxiosFetcherする形に修正

--- frontend/app/app/hooks/swr/useGetMetaData.ts
+++ frontend/app/app/hooks/swr/useGetMetaData.ts
@@ -1,22 +1,15 @@
-import useSWR from "swr";
-
-import api from "@/app/libs/axios/api";
 import { URLMetaData } from "@/app/types/axios/metaData";
+
+import { useSWRWithAxiosFetcher } from "./useSWRWithAxiosFetcher";
 
 export const useGetURLMetaData = (url: string) => {
   const URI = encodeURI(url);
   const endPointWithParams = `/meta_data?meta_data[url]=${URI}`;
 
-  const fetcher = async (endPoint: string) => {
-    const response = await api.get<URLMetaData>(endPoint);
-    return response.data;
-  };
-
-  const { data, error, isLoading } = useSWR<URLMetaData, Error>(
+  const { responseData, error, isLoading } = useSWRWithAxiosFetcher<URLMetaData>(
     endPointWithParams,
-    fetcher,
     { dedupingInterval: 86400000, errorRetryCount: 0 }
   );
 
-  return { data, error, isLoading };
+  return { URLMetaData: responseData, error, isLoading };
 };
