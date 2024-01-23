786d780d8835406111fb8a9a8c491b6d3883c99c
Satoshi170 <sato1704@outlook.jp>
2024-01-23 15:34:36 +0900

    useSWRWithAxiosFetcherのエラーハンドリングを実装

--- frontend/app/app/hooks/swr/useSWRWithAxiosFetcher.ts
+++ frontend/app/app/hooks/swr/useSWRWithAxiosFetcher.ts
@@ -1,17 +1,33 @@
-import { AxiosError } from "axios";
+import { isAxiosError } from "axios";
 import useSWR, { SWRConfiguration } from "swr";
 
 import api from "@/app/libs/axios/api";
+
+import { useSetToastState } from "../recoil/toastState/useSetToastState";
 
 export const useSWRWithAxiosFetcher = <T>(
   endPoint: string,
   options?: SWRConfiguration
 ) => {
+  const { setSeverErrorToast } = useSetToastState();
   const fetcher = async (endPoint: string) => {
     const response = await api.get<T>(endPoint);
     return response.data;
   };
 
-  const { data, error, ...other } = useSWR<T, AxiosError>(endPoint, fetcher, options);
-  return { responseData: data, error, ...other };
+  const { data, error, ...other } = useSWR<T, unknown>(endPoint, fetcher, options);
+  let errorStatus: number | null = null;
+
+  if (error) {
+    if (isAxiosError(error)) {
+      errorStatus = error.response?.status ?? null;
+      if (!errorStatus) {
+        setSeverErrorToast();
+      }
+    } else {
+      setSeverErrorToast();
+    }
+  }
+
+  return { responseData: data, error, errorStatus, ...other };
 };
