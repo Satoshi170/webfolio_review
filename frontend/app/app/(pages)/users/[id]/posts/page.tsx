7ea6dcf5160dd70c74c25db5c4f363bbab56c699
Satoshi170 <sato1704@outlook.jp>
2024-01-28 21:41:04 +0900

    /users/:id/postsページでエラー時とデータがない時に適切な画面を表示するように修正

--- frontend/app/app/(pages)/users/[id]/posts/page.tsx
+++ frontend/app/app/(pages)/users/[id]/posts/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Text } from "@chakra-ui/react";
 import Error from "next/error";
 import { usePathname } from "next/navigation";
 
@@ -7,13 +8,29 @@ import GoBackLink from "@/app/components/atoms/GoBackLink";
 import LoadingSpinner from "@/app/components/atoms/LoadingSpinner";
 import PostCard from "@/app/components/organisms/posts/PostCard";
 import CenteredBox from "@/app/components/styledWrappers/CenteredBox";
-import { useGetUserPortfoliosByUserIdOperation } from "@/app/hooks/operations/portfolio/userPortfolio/useGetUserPortfoliosByUserIdOperation";
+import { useGetUserPortfoliosByUserId } from "@/app/hooks/swr/portfolio/userPortfolio/useGetUserPortfoliosByUserId";
+import { getIdOrTriggerNotFound } from "@/app/utils/getIdOrTriggerNotFound";
 
 const UserIdPostsPage: React.FC = () => {
   const pathname = usePathname();
-  const { status, portfoliosData } = useGetUserPortfoliosByUserIdOperation(pathname);
+  const newPathname = pathname.replace(/\/posts$/, "");
+  const id = getIdOrTriggerNotFound({ pathname: newPathname, routeKey: "users" });
+  const { portfoliosData, error, errorStatus, isLoading } =
+    useGetUserPortfoliosByUserId(id);
 
-  if (!status) {
+  if (errorStatus) {
+    return <Error statusCode={errorStatus} />;
+  }
+
+  if (error || !portfoliosData) {
+    return (
+      <CenteredBox>
+        <GoBackLink />
+      </CenteredBox>
+    );
+  }
+
+  if (isLoading) {
     return (
       <CenteredBox>
         <LoadingSpinner />
@@ -21,18 +38,23 @@ const UserIdPostsPage: React.FC = () => {
     );
   }
 
-  if (status === 200 && portfoliosData) {
+  if (portfoliosData && portfoliosData.length == 0) {
     return (
       <CenteredBox>
         <GoBackLink />
-        {portfoliosData.map((portfolioData, i) => (
-          <PostCard portfolioData={portfolioData} linkOptions={{ body: true }} key={i} />
-        ))}
+        <Text>まだ投稿がありません</Text>
       </CenteredBox>
     );
-  } else {
-    return <Error statusCode={status} />;
   }
+
+  return (
+    <CenteredBox>
+      <GoBackLink />
+      {portfoliosData.map((portfolioData, i) => (
+        <PostCard portfolioData={portfolioData} linkOptions={{ body: true }} key={i} />
+      ))}
+    </CenteredBox>
+  );
 };
 
 export default UserIdPostsPage;
