ecb58dec7a1d908566f0b5bfc312b311c228c24f
Satoshi170 <sato1704@outlook.jp>
2024-01-31 01:43:36 +0900

    今回のブランチの作業の結果不要になったものを削除

--- frontend/app/app/stores/atoms/portfolioDataState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-import { PortfolioData } from "@/app/types/axios/portfolio/portfolioData";
-
-export const portfolioDataState = atom<PortfolioData[]>({
-  key: "portfolioData",
-  default: []
-});
