707ceda08f72d66ea4ac655d63b51388ee070a56
Satoshi170 <sato1704@outlook.jp>
2024-01-30 14:22:18 +0900

    useGetUserPortfoliosByUserId関連の型定義を修正

--- frontend/app/app/hooks/swr/portfolio/userPortfolio/useGetUserPortfoliosByUserId.ts
+++ frontend/app/app/hooks/swr/portfolio/userPortfolio/useGetUserPortfoliosByUserId.ts
@@ -1,11 +1,11 @@
-import { GetPortfoliosResponse } from "@/app/types/axios/portfolio/getPortfolios";
+import { GetUserPortfoliosResponse } from "@/app/types/axios/portfolio/user_portfolio/getUserPortfolioByUserId";
 
 import { useSWRWithAxiosFetcher } from "../../useSWRWithAxiosFetcher";
 
 export const useGetUserPortfoliosByUserId = (userId: number) => {
   const endPoint = "/user_portfolios";
   const params = { userId };
-  const { responseData, ...other } = useSWRWithAxiosFetcher<GetPortfoliosResponse>(
+  const { responseData, ...other } = useSWRWithAxiosFetcher<GetUserPortfoliosResponse>(
     [endPoint, params],
     {
       errorRetryCount: 2
