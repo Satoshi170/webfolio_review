7f5b0530338edf8f3b57f83c202d8a1fd5d20d27
Satoshi170 <sato1704@outlook.jp>
2024-02-03 09:32:43 +0900

    フロント側で取得するPortfolioDataの型を修正

---
+++ frontend/app/app/constants/datas/portfolios/operationStatuses.ts
@@ -0,0 +1,5 @@
+export const operationStatuses = [
+  { key: 0, value: "active" },
+  { key: 1, value: "maintenance" },
+  { key: 2, value: "inactive" }
+] as const;
--- frontend/app/app/types/axios/portfolio/portfolioData.ts
+++ frontend/app/app/types/axios/portfolio/portfolioData.ts
@@ -1,3 +1,5 @@
+import { operationStatuses } from "@/app/constants/datas/portfolios/operationStatuses";
+
 import { CommentData } from "./comment/comment";
 import { UserData } from "../../auth";
 
@@ -5,10 +7,15 @@ interface Good {
   userId: number;
 }
 
+type OperationStatus = (typeof operationStatuses)[number]["value"];
+
 export interface PortfolioData {
   id: number;
   title: string;
   content: string;
+  operationStatus: OperationStatus;
+  portfolioSiteUrl: string;
+  repositoryUrl: string;
   updatedAt: Date;
   user: UserData;
   goods: Good[];
