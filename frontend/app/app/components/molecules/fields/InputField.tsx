ca777b828b0e917d924c893b9b69972112897343
Satoshi170 <sato1704@outlook.jp>
2024-02-07 18:59:00 +0900

    各fieldコンポーネントでisRequiredプロパティを受け取れるようにしFormControlコンポーネントのisRequiredを利用するように変更

--- frontend/app/app/components/molecules/fields/InputField.tsx
+++ frontend/app/app/components/molecules/fields/InputField.tsx
@@ -3,32 +3,26 @@
 import { FormControl, FormErrorMessage, FormLabel, Input } from "@chakra-ui/react";
 import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-import RequiredAsterisk from "../../atoms/RequiredAsterisk";
-
 export interface InputFieldProps<T extends FieldValues> {
   name: Path<T>;
   label: string;
-  isRequired?: boolean;
-  type?: string;
   register: UseFormRegister<T>;
   error?: FieldError;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  type?: string;
+  isRequired?: boolean;
 }
 
 const InputField = <T extends FieldValues>({
   name,
   label,
-  isRequired = false,
-  type = "text",
   register,
-  error
+  error,
+  type = "text",
+  isRequired = false
 }: InputFieldProps<T>) => {
   return (
-    <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={name as string}>
-        {label}
-        {isRequired && <RequiredAsterisk />}
-      </FormLabel>
+    <FormControl isInvalid={!!error} isRequired={isRequired}>
+      <FormLabel htmlFor={name as string}>{label}</FormLabel>
       <Input type={type} id={name as string} {...register(name)} />
       <FormErrorMessage>{error?.message}</FormErrorMessage>
     </FormControl>
--- frontend/app/app/components/molecules/fields/TextareaField.tsx
+++ frontend/app/app/components/molecules/fields/TextareaField.tsx
@@ -3,29 +3,24 @@
 import { FormControl, FormErrorMessage, FormLabel, Textarea } from "@chakra-ui/react";
 import { FieldError, FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-import RequiredAsterisk from "../../atoms/RequiredAsterisk";
-
 export interface TextareaFieldProps<T extends FieldValues> {
   name: Path<T>;
   label: string;
-  isRequired?: boolean;
   register: UseFormRegister<T>;
   error?: FieldError;
+  isRequired?: boolean;
 }
 
 const TextareaField = <T extends FieldValues>({
   name,
   label,
-  isRequired = false,
   register,
-  error
+  error,
+  isRequired = false
 }: TextareaFieldProps<T>) => {
   return (
-    <FormControl isInvalid={!!error}>
-      <FormLabel>
-        {label}
-        {isRequired && <RequiredAsterisk />}
-      </FormLabel>
+    <FormControl isInvalid={!!error} isRequired={isRequired}>
+      <FormLabel>{label}</FormLabel>
       <Textarea id={name as string} {...register(name)} h="200px"></Textarea>
       <FormErrorMessage>{error?.message}</FormErrorMessage>
     </FormControl>
