934400dde1c14e6edf8fe5ab06a95c9d53f6659a
Satoshi170 <sato1704@outlook.jp>
2024-02-10 19:54:22 +0900

    operationStatusesからOptionsを生成。 labelからvalueを取得する関数を作成。

--- frontend/app/app/constants/datas/portfolios/operationStatuses.ts
+++ frontend/app/app/constants/datas/portfolios/operationStatuses.ts
@@ -3,3 +3,15 @@ export const operationStatuses = [
   { key: 1, value: "maintenance" },
   { key: 2, value: "inactive" }
 ] as const;
+
+type OperationStatusValue = (typeof operationStatuses)[number]["value"];
+
+export const operationStatusOptions = operationStatuses.map((item) => ({
+  value: String(item.key),
+  label: item.value
+}));
+
+export const getValueByLabel = (value: OperationStatusValue) => {
+  const option = operationStatuses.find((status) => status.value === value);
+  return String(option!.key);
+};
