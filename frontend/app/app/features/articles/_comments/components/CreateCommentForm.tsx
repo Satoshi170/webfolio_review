329bf99f21fdc91f9e509032cf82405e0b6243cd
Satoshi170 <sato1704@outlook.jp>
2024-02-29 16:44:51 +0900

    CreateCommentFormを作成

---
+++ frontend/app/app/features/articles/_comments/components/CreateCommentForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Box, Card, CardBody, CardFooter } from "@chakra-ui/react";
+
+import SubmitButton from "@/app/components/atoms/SubmitButton";
+import SelectBoxField from "@/app/components/molecules/fields/SelectBoxField";
+import TextareaField from "@/app/components/molecules/fields/TextareaField";
+
+import { commentTagOptions } from "./commentTagOptions";
+import { useCreateCommentForm } from "../hooks/useCreateCommentForm";
+
+import type { ArticleData } from "../../types/articleData";
+
+interface Props {
+  articleData: ArticleData;
+}
+
+const CreateCommentForm: React.FC<Props> = ({ articleData }) => {
+  const { register, control, isLoading, isValid, errors, handleFormSubmit } =
+    useCreateCommentForm(articleData.id);
+
+  return (
+    <Card rounded="none" p="3">
+      <form onSubmit={handleFormSubmit}>
+        <CardBody>
+          <TextareaField
+            name="content"
+            placeholder="コメントする"
+            register={register}
+            error={errors.content}
+          />
+          <Box my="3" w="50%">
+            <SelectBoxField
+              name="tagIds"
+              options={commentTagOptions}
+              placeholder="タグを選択(任意)"
+              control={control}
+              error={errors.tagIds}
+              isMulti
+            />
+          </Box>
+        </CardBody>
+        <CardFooter>
+          <SubmitButton text="投稿する" isLoading={isLoading} isDisabled={!isValid} />
+        </CardFooter>
+      </form>
+    </Card>
+  );
+};
+
+export default CreateCommentForm;
