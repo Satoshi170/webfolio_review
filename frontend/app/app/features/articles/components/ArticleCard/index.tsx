a827e8f3292cbb318d3e1cc3390aed43ef588823
Satoshi170 <sato1704@outlook.jp>
2024-02-29 13:16:38 +0900

    新しくArticleCardコンポーネントを作成 それをグリッド表示するArticlesCardsGridListを作成

---
+++ frontend/app/app/features/articles/components/ArticleCard/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import NextLink from "next/link";
+
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Icon,
+  Text
+} from "@chakra-ui/react";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import UpdatedDateText from "@/app/components/atoms/texts/UpdatedDateText";
+import UserIcon from "@/app/components/atoms/users/UserIcon";
+
+import type { ArticleData } from "../../types/articleData";
+
+interface Props {
+  articleData: ArticleData;
+}
+
+const ArticleCard: React.FC<Props> = ({ articleData }) => {
+  const { id, title, updatedAt, user, goods } = articleData;
+
+  return (
+    <Card w="full" h="auto" as={NextLink} href={`articles/${id}`}>
+      <CardHeader>
+        <Flex flex="1" gap="2" alignItems="center">
+          <UserIcon image={user.image} name={user.name} diameter={30} />
+          <Heading fontSize="md">{user.name}</Heading>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Heading fontSize="lg">{title}</Heading>
+      </CardBody>
+      <CardFooter alignItems="center" justifyContent="space-between">
+        <Flex alignItems="center" color="blackAlpha.500" gap="1">
+          <Icon as={AiOutlineHeart} />
+          <Text>{goods.length}</Text>
+        </Flex>
+        <UpdatedDateText date={updatedAt} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ArticleCard;
---
+++ frontend/app/app/features/articles/components/ArticleCardsGridList/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Grid } from "@chakra-ui/react";
+
+import ArticleCard from "../ArticleCard";
+
+import type { ArticleData } from "../../types/articleData";
+
+interface Props {
+  articlesData: ArticleData[];
+}
+
+const ArticleCardsGridList: React.FC<Props> = ({ articlesData }) => {
+  return (
+    <Grid
+      w="full"
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(3, 1fr)"
+      }}
+      gap={2}
+    >
+      {articlesData.map((articleData, i) => (
+        <ArticleCard articleData={articleData} key={i} />
+      ))}
+    </Grid>
+  );
+};
+
+export default ArticleCardsGridList;
