ad372c51df860b0d2af3da60895c6ef661d905df
Satoshi170 <sato1704@outlook.jp>
2024-03-01 17:59:33 +0900

    components/atomsにlinksフォルダを作成。 BlueLinkButtonとGoBackLinkをそこに移動

--- frontend/app/app/components/molecules/actionButtons/auth/SignInLinkButton.tsx
+++ frontend/app/app/components/molecules/actionButtons/auth/SignInLinkButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BlueLinkButton from "@/app/components/atoms/BlueLinkButton";
+import BlueLinkButton from "@/app/components/atoms/links/BlueLinkButton";
 
 const SignInLinkButton: React.FC = () => {
   return <BlueLinkButton href="/auth/sign_in" text="サインイン" />;
