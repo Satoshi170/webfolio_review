6036944f4d2e306ebc2f7a01558ee3376a2f0c45
Satoshi170 <sato1704@outlook.jp>
2024-03-10 23:26:29 +0900

    型MyDataをUserDataから継承して作成するように修正

--- frontend/app/app/types/auth.ts
+++ frontend/app/app/types/auth.ts
@@ -1,17 +1,14 @@
 import type { UnauthorizedResponseDataSchema } from "../libs/zod/apiErrorResponses/auth/responseDataSchema";
 import type { z } from "zod";
 
-export interface MyData {
-  id: number;
-  name: string;
-  role: "user" | "guest";
-  image: string;
-}
-
 export interface UserData {
   id: number;
   name: string;
   image: string;
+}
+
+export interface MyData extends UserData {
+  role: "user" | "guest";
 }
 
 export type UnauthorizedResponseData = z.infer<typeof UnauthorizedResponseDataSchema>;
