05026b49767b1be2e096b57fc3bfdf90210deaad
Satoshi170 <sato1704@outlook.jp>
2024-03-11 23:26:43 +0900

    ArticleHeader, ArticleBody, ArticleFooterを作成

---
+++ frontend/app/app/features/articles/components/ArticleDetailPage/Article/Footer/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { CardFooter, Flex, Icon, Text } from "@chakra-ui/react";
+import { BiComment } from "react-icons/bi";
+
+import LikeButton from "@/app/features/articles/_likes/components/layouts/LikeButton";
+import { useToggleLikeArticleGood } from "@/app/features/articles/_likes/hooks/useToggleArticleGood";
+import { useArticleData } from "@/app/features/articles/hooks/useArticleData";
+import { UpdatedDateText } from "@/app/components/atoms";
+
+import OperationStatusBadge from "../../OperationStatusBadge";
+
+const ArticleFooter: React.FC = () => {
+  const articleData = useArticleData();
+  const { isLiked, toggleLike, totalLiked } = useToggleLikeArticleGood(articleData);
+  const { operationStatus, totalComments, updatedAt } = articleData;
+
+  return (
+    <CardFooter alignItems="center" justifyContent="space-between">
+      <Flex gap="3" alignItems="center">
+        <LikeButton onClick={toggleLike} isLiked={isLiked} totalLiked={totalLiked} />
+        <Flex alignItems="center" color="gray.500" gap="2">
+          <Icon as={BiComment} />
+          <Text>{totalComments}</Text>
+        </Flex>
+      </Flex>
+      <Flex gap="3">
+        <UpdatedDateText date={updatedAt} />
+        <OperationStatusBadge value={operationStatus} />
+      </Flex>
+    </CardFooter>
+  );
+};
+
+export default ArticleFooter;
