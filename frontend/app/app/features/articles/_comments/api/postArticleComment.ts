aeb88e40fb8e8e06ae0a086b1e848eee212b7fe0
Satoshi170 <sato1704@outlook.jp>
2024-03-14 02:50:28 +0900

    コメント作成時にコメント詳細をレスポンスするように変更

--- frontend/app/app/features/articles/_comments/api/postArticleComment.ts
+++ frontend/app/app/features/articles/_comments/api/postArticleComment.ts
@@ -1,15 +1,17 @@
 import api from "@/app/libs/axios/api";
 import addAuthInfoToRequest from "@/app/libs/cookie/loadAuthInfo";
 
+import type { CommentData } from "../types";
 import type { CommentParams } from "../types/api";
 
 export const postArticleComment = async (
   articleId: number,
   params: CommentParams
-): Promise<void> => {
-  await api.post(
+): Promise<CommentData> => {
+  const response = await api.post<CommentData>(
     `/articles/${articleId}/comments`,
     { comment: params },
     addAuthInfoToRequest({})
   );
+  return response.data;
 };
