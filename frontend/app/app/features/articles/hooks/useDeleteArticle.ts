77e6600154e35029ae4d9c317c85078711a14592
Satoshi170 <sato1704@outlook.jp>
2024-03-14 18:16:46 +0900

    投稿削除成功時に自身の投稿ページに移動するように変更

--- frontend/app/app/features/articles/hooks/useDeleteArticle.ts
+++ frontend/app/app/features/articles/hooks/useDeleteArticle.ts
@@ -1,5 +1,7 @@
 import { useCallback } from "react";
+import { useRouter } from "next/navigation";
 
+import { useGetLoginState } from "@/app/hooks/recoil/loginState/useGetLoginState";
 import { useSetToastState } from "@/app/hooks/recoil/toastState/useSetToastState";
 import { resolveErrorMessage } from "@/app/utils/resolveErrorMessage";
 
@@ -7,19 +9,22 @@ import { deleteArticle } from "../api/deleteArticle";
 
 export const useDeleteArticle = () => {
   const { setSuccessToast, setErrorToast } = useSetToastState();
+  const { userData } = useGetLoginState();
+  const router = useRouter();
 
   const deleteArticleOperation = useCallback(
     async (id: number) => {
       try {
         await deleteArticle(id);
-        window.location.reload();
+        const redirectEndpoint = userData ? `/users/${userData.id}/articles` : "/";
+        router.push(redirectEndpoint);
         setSuccessToast("投稿の削除に成功しました");
       } catch (e) {
         const errorMessage = resolveErrorMessage(e);
         setErrorToast(errorMessage);
       }
     },
-    [setSuccessToast, setErrorToast]
+    [setSuccessToast, setErrorToast, router, userData]
   );
 
   return deleteArticleOperation;
