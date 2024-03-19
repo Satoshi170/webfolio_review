84bc2928e699a3aae0c23e46c968efb79e175194
Satoshi170 <sato1704@outlook.jp>
2024-03-19 22:31:08 +0900

    コメントの並び順を変えるuseSortCommentsを作成

---
+++ frontend/app/app/features/articles/hooks/useSortComments.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import type { CommentData } from "../_comments/types";
+
+export const useSortComments = (initCommentsData: CommentData[] | []) => {
+  const [comments, setComments] = useState(initCommentsData);
+
+  const sortOrder = (sortOrder: "asc" | "desc") => {
+    if (sortOrder == "asc") {
+      const newCommentsData = [...initCommentsData].sort(
+        (x, y) => x.updatedAt.getTime() - y.updatedAt.getTime()
+      );
+      setComments(newCommentsData);
+    } else if (sortOrder == "desc") {
+      setComments(initCommentsData);
+    } else {
+      throw new TypeError();
+    }
+  };
+
+  return { sortedComments: comments, sortOrder };
+};
