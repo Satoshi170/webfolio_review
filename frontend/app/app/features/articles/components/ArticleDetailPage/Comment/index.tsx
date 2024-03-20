16432dd82098f211f6c93364dd565d53c2c79e13
Satoshi170 <sato1704@outlook.jp>
2024-03-20 10:10:00 +0900

    useSortCommentsを用いてコメントをソートできるようにMenuコンポーネントを使用

--- frontend/app/app/features/articles/components/ArticleDetailPage/Comment/index.tsx
+++ frontend/app/app/features/articles/components/ArticleDetailPage/Comment/index.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-import { Box, Divider, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  HStack,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup
+} from "@chakra-ui/react";
+import { MdSort } from "react-icons/md";
 
 import CommentsList from "../../../_comments/components/CommentsList";
 import CreateCommentForm from "../../../_comments/components/CreateCommentForm";
+import { useSortComments } from "../../../hooks/useSortComments";
 
 import type { CommentData } from "../../../_comments/types";
 
@@ -12,13 +25,36 @@ interface Props {
 }
 
 const CommentSection: React.FC<Props> = ({ commentsData }) => {
+  const { sortedComments, sortOrder } = useSortComments(commentsData);
+
   return (
     <Box bg="white">
-      <Heading as="h3" textAlign="center" size="md" my="4">
-        コメント
-      </Heading>
+      <HStack my="4">
+        <Heading as="h3" textAlign="center" size="md" flex="1">
+          コメント
+        </Heading>
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={IconButton}
+            aria-label="comment-options"
+            icon={<MdSort />}
+            variant="unstyled"
+          />
+          <MenuList>
+            <MenuOptionGroup
+              type="radio"
+              defaultValue="desc"
+              onChange={(val) => sortOrder(val as "asc" | "desc")}
+              title="並び替え"
+            >
+              <MenuItemOption value="desc">最新順</MenuItemOption>
+              <MenuItemOption value="asc">古い順</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </HStack>
       <Divider />
-      <CommentsList commentsData={commentsData} />
+      <CommentsList commentsData={sortedComments} />
       <Divider />
       <CreateCommentForm />
     </Box>
