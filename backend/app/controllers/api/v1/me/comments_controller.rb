ab12cde526c3159223fe00232217f64a1a14599c
Satoshi170 <sato1704@outlook.jp>
2024-03-20 13:34:40 +0900

    "/me/comments"のレスポンスをupdated_atの降順に変更

--- backend/app/controllers/api/v1/me/comments_controller.rb
+++ backend/app/controllers/api/v1/me/comments_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::Me::CommentsController < ApplicationController
   before_action :authenticate_api_v1_user!, only: [:index]
 
   def index
-    comments = current_api_v1_user.comments.includes(:article, :tags)
+    comments = current_api_v1_user.comments.includes(:article, :tags).
+      order(updated_at: :desc)
     render json: Me::CommentResource.new(comments).serializable_hash,
            status: :ok
   end
