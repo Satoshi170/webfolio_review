3e15ed7372587c0d61907661fd01045874fb9dbc
Satoshi170 <sato1704@outlook.jp>
2024-03-23 15:05:44 +0900

    articleのfactoryをfakerを使用するように変更

--- backend/spec/factories/articles.rb
+++ backend/spec/factories/articles.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :article do
-    title { "testTitle" }
-    content { "testContent" }
+    title { Faker::Lorem.sentence(word_count: 3).truncate(25) }
+    content { Faker::Lorem.paragraph(sentence_count: 5).truncate(255) }
     operation_status { "active" }
-    portfolio_site_url { "http://example.com" }
-    repository_url { nil }
+    portfolio_site_url { Faker::Internet.url }
+    repository_url { Faker::Internet.url }
     user { nil }
   end
 end
