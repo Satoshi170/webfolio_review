e57d6c8bce82fa57d6efd32cd56ee52b789c46e6
Satoshi170 <sato1704@outlook.jp>
2024-03-23 20:41:02 +0900

    環境ごとに別のseedファイルを読み込めるように設定

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,2 +1,8 @@
 Tag.create(name: "bug report")
 Tag.create(name: "suggestion")
+
+env_seed_file = Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb")
+
+if File.exist?(env_seed_file)
+  load(env_seed_file)
+end
