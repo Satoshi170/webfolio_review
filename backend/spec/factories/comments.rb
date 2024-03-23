756c7637cdcba34fd1829eb0bc9c626f2ab5faee
Satoshi170 <sato1704@outlook.jp>
2024-03-23 16:37:12 +0900

    commentのfactoryでアサーションを適切に設定できるように修正

--- backend/spec/factories/comments.rb
+++ backend/spec/factories/comments.rb
@@ -3,6 +3,15 @@ FactoryBot.define do
     user { nil }
     article { nil }
     content { Faker::Lorem.paragraph(sentence_count: 5).truncate(255) }
-    tag_ids { nil }
+
+    transient do
+      tags { [] }
+    end
+
+    after(:create) do |comment, evaluator|
+      evaluator.tags.each do |tag|
+        comment.tags << tag
+      end
+    end
   end
 end
