e6100ca1ba153f571ea78b95f5632a71a3449743
Satoshi170 <sato1704@outlook.jp>
2024-03-23 15:12:04 +0900

    userのfactoryのoperation_statusがランダムになるように設定

--- backend/spec/factories/articles.rb
+++ backend/spec/factories/articles.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :article do
     title { Faker::Lorem.sentence(word_count: 3).truncate(25) }
     content { Faker::Lorem.paragraph(sentence_count: 5).truncate(255) }
-    operation_status { "active" }
+    operation_status { Article.operation_status.values.sample }
     portfolio_site_url { Faker::Internet.url }
     repository_url { Faker::Internet.url }
     user { nil }
