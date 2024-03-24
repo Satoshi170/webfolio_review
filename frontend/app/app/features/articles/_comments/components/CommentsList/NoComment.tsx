f0d90eed3eee170cb1bc147a5c9c061cd39fa0e8
Satoshi170 <sato1704@outlook.jp>
2024-03-25 07:48:22 +0900

    コメントがない時に表示するNoCommentコンポーネントを作成

---
+++ frontend/app/app/features/articles/_comments/components/CommentsList/NoComment.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Text } from "@chakra-ui/react";
+
+const NoComment: React.FC = () => {
+  return (
+    <Text p="12" w="full" textAlign="center">
+      コメントはありません
+    </Text>
+  );
+};
+
+export default NoComment;
