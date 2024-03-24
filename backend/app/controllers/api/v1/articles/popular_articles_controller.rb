0064d344c9d144bb3a9a3a7c662afd6ed3a4376d
Satoshi170 <sato1704@outlook.jp>
2024-03-24 16:13:43 +0900

    GET "/latest_articles"で返却されるデータの数を20に修正

--- backend/app/controllers/api/v1/articles/popular_articles_controller.rb
+++ backend/app/controllers/api/v1/articles/popular_articles_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::Articles::PopularArticlesController < ApplicationController
   def index
-    articles = Article.includes(:comments, user: { image_attachment: :blob }).
+    articles = Article.includes(user: { image_attachment: :blob }).
       left_joins(:goods).
       group(:id).
       order('COUNT(goods.id) DESC').
-      limit(3)
+      limit(20)
 
     render json: Articles::ArticleResource.new(articles).serializable_hash,
            status: :ok
