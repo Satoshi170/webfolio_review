074c896ae46e1c22919bee1cb4ca6ab983ccc0d9
Satoshi170 <sato1704@outlook.jp>
2024-03-24 16:10:20 +0900

    GET "/latest_articles"で返却されるデータの数を10に修正

--- backend/app/controllers/api/v1/articles/latest_articles_controller.rb
+++ backend/app/controllers/api/v1/articles/latest_articles_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Articles::LatestArticlesController < ApplicationController
   def index
-    articles = Article.includes(:comments, user: { image_attachment: :blob }).
+    articles = Article.includes(user: { image_attachment: :blob }).
       order(updated_at: :desc).
-      limit(3)
+      limit(10)
 
     render json: Articles::ArticleResource.new(articles).serializable_hash,
            status: :ok
