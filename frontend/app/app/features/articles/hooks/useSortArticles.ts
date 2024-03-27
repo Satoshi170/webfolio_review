37957ba190cfc2ff654c476c7ef7fac62b97956f
Satoshi170 <sato1704@outlook.jp>
2024-03-27 10:51:01 +0900

    useSortArticlesに除外フィルタ機能を追加

--- frontend/app/app/features/articles/hooks/useSortArticles.ts
+++ frontend/app/app/features/articles/hooks/useSortArticles.ts
@@ -1,36 +1,40 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 import type { ArticleData } from "../types/articleData";
 
 export const useSortArticles = (initArticleData: ArticleData[]) => {
   const [articles, setArticles] = useState(initArticleData);
 
-  const sortOrder = useCallback(
-    (sortOrder: "asc" | "desc" | "popular") => {
-      switch (sortOrder) {
-        case "asc": {
-          const newArticlesData = [...initArticleData].sort(
-            (x, y) => x.updatedAt.getTime() - y.updatedAt.getTime()
-          );
-          setArticles(newArticlesData);
-          break;
-        }
-        case "desc":
-          setArticles(initArticleData);
-          break;
-        case "popular": {
-          const newArticlesData = [...initArticleData].sort(
-            (x, y) => y.totalLikes - x.totalLikes
-          );
-          setArticles(newArticlesData);
-          break;
-        }
-        default:
-          throw new TypeError();
-      }
-    },
-    [initArticleData]
-  );
+  const setExcludeFilter = (val: ("maintenance" | "inactive")[]) => {
+    if (val.length == 0) {
+      setArticles(initArticleData);
+    } else {
+      const newArticles = initArticleData.filter(
+        (data) => !val.includes(data.operationStatus)
+      );
+      setArticles(newArticles);
+    }
+  };
 
-  return { sortedArticles: articles, sortOrder };
+  const sortOrder = (sortOrder: "asc" | "desc" | "popular") => {
+    switch (sortOrder) {
+      case "asc":
+        setArticles((prev) =>
+          [...prev].sort((x, y) => x.updatedAt.getTime() - y.updatedAt.getTime())
+        );
+        break;
+      case "desc":
+        setArticles((prev) =>
+          [...prev].sort((x, y) => y.updatedAt.getTime() - x.updatedAt.getTime())
+        );
+        break;
+      case "popular":
+        setArticles((prev) => [...prev].sort((x, y) => y.totalLikes - x.totalLikes));
+        break;
+      default:
+        throw new TypeError();
+    }
+  };
+
+  return { sortedArticles: articles, sortOrder, setExcludeFilter };
 };
