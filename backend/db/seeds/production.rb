4215beb1522fe8b99581518123ad8e2c8e521638
Satoshi170 <sato1704@outlook.jp>
2024-03-29 01:23:59 +0900

    本番用のseedデータを作成

---
+++ backend/db/seeds/production.rb
@@ -0,0 +1,33 @@
+include FactoryBot::Syntax::Methods
+
+user1 = create(:user, name: "Satoshi170")
+article1 = create(:article,
+  title: "ポートフォリオを共有できるポートフォリオを作成しました!",
+  content: "未経験エンジニア同士がポートフォリオを共有できるWebサイトを作成しました!",
+  operation_status: "active",
+  portfolio_site_url: "https://webfolio-review.com/",
+  repository_url: "https://github.com/Satoshi170/webfolio_review",
+  user: user1
+)
+
+users = create_list(:user, 30)
+
+users.each do |user|
+  create(:good, user: user, article: article1)
+end
+
+articles = Array.new(50) do
+  create(:article, user: users.sample)
+end
+
+comments = Array.new(100) do
+  tags = Tag.all.sample(rand(0..2))
+  create(:comment, user: users.sample, article: articles.sample, tags: tags)
+end
+
+users.each do |user|
+  good_articles = articles.sample(rand(50))
+  good_articles.each do |article|
+    create(:good, user: user, article: article)
+  end
+end
