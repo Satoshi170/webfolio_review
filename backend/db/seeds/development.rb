7d15e61bec312c4561e000a5d8be590dc2d15866
Satoshi170 <sato1704@outlook.jp>
2024-03-28 18:07:18 +0900

    特殊なseedデータを作成

--- backend/db/seeds/development.rb
+++ backend/db/seeds/development.rb
@@ -1,6 +1,20 @@
 include FactoryBot::Syntax::Methods
 
+user1 = create(:user, name: "Satoshi170")
+article1 = create(:article,
+  title: "ポートフォリオを共有できるポートフォリオを作成しました!",
+  content: "未経験エンジニア同士がポートフォリオを共有できるWebサイトを作成しました!",
+  operation_status: "active",
+  portfolio_site_url: "https://webfolio-review.com/",
+  repository_url: "https://github.com/Satoshi170/webfolio_review",
+  user: user1
+)
+
 users = create_list(:user, 10)
+
+users.each do |user|
+  create(:good, user: user, article: article1)
+end
 
 articles = Array.new(50) do
   create(:article, user: users.sample)
