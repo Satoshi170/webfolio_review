1cd8b51aee58f46759d71a107d31e128ea5bb66a
Satoshi170 <sato1704@outlook.jp>
2024-03-28 17:52:11 +0900

    titleのバリデーションの変更によりArticleのFactoryも変更

--- backend/spec/factories/articles.rb
+++ backend/spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :article do
-    title { Faker::Lorem.sentence(word_count: 3).truncate(25) }
+    title { Faker::Lorem.sentence(word_count: 11).truncate(75) }
     content { Faker::Lorem.paragraph(sentence_count: 5).truncate(255) }
     operation_status { Article.operation_status.values.sample }
     portfolio_site_url { Faker::Internet.url }
