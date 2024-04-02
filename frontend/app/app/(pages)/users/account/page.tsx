61b6687d3f9c278e96716dba873ea77e683674d9
Satoshi170 <sato1704@outlook.jp>
2024-04-02 15:51:30 +0900

    ゲストユーザーにDeleteAccountButtonWithModalがレンダリングされないように変更

--- frontend/app/app/(pages)/users/account/page.tsx
+++ frontend/app/app/(pages)/users/account/page.tsx
@@ -4,10 +4,19 @@ import { Box, Spacer, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui
 
 import WithRedirectIfLoggedOut from "@/app/features/auth/accessControl/page/WithRedirectIfLoggedOut";
 import DeleteAccountButtonWithModal from "@/app/features/auth/deleteAccount/DeleteAccountButtonWithModal";
+import { useGetLoginState } from "@/app/hooks/recoil/loginState/useGetLoginState";
 import { GoBackLink } from "@/app/components/atoms";
 import AccountInformationTabPanel from "@/app/components/organisms/users/AccountInformationTab";
 
 const UserAccountPage: React.FC = () => {
+  const { isLogin, userData } = useGetLoginState();
+
+  if (!isLogin) {
+    return null;
+  }
+
+  const isGuestUser = userData.role == "guest";
+
   return (
     <Box flex="1" mx="auto" boxShadow="md" p="12" rounded="md">
       <GoBackLink />
@@ -17,9 +26,9 @@ const UserAccountPage: React.FC = () => {
         </TabList>
         <TabPanels>
           <TabPanel textAlign="center">
-            <AccountInformationTabPanel />
+            <AccountInformationTabPanel userData={userData} isGuestUser={isGuestUser} />
             <Spacer my="5" />
-            <DeleteAccountButtonWithModal />
+            {!isGuestUser && <DeleteAccountButtonWithModal />}
           </TabPanel>
         </TabPanels>
       </Tabs>
