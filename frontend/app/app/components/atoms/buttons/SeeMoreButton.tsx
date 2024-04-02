3d71371bc3547c09905227c5565e159cb56db7e9
Satoshi170 <sato1704@outlook.jp>
2024-04-02 11:48:34 +0900

    SeeMoreButtonでのログインしてるかどうかの認証部分を撤廃

--- frontend/app/app/components/atoms/buttons/SeeMoreButton.tsx
+++ frontend/app/app/components/atoms/buttons/SeeMoreButton.tsx
@@ -2,16 +2,14 @@
 
 import { Button } from "@chakra-ui/react";
 
-import WithSignInAlert from "@/app/features/auth/accessControl/button/WithSignInAlert";
-
 interface Props {
   onClick: () => void;
 }
 
-export const SeeMoreButton: React.FC<Props> = WithSignInAlert(({ onClick }) => {
+export const SeeMoreButton: React.FC<Props> = ({ onClick }) => {
   return (
     <Button onClick={onClick} w="full" textAlign="center" variant="link" my="3">
       もっと見る
     </Button>
   );
-});
+};
